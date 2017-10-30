75df3dae2b9c8c269e64c20f7fb0daaedebc9196
Haris Ghauri <harisghauri@Hariss-MacBook-Pro.local>
2017-10-30 00:50:45 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDPm6nFp3x8zyWBgEzTcApGWRTHDBkxFrE",
+    authDomain: "aquaponics-system.firebaseapp.com",
+    databaseURL: "https://aquaponics-system.firebaseio.com",
+    projectId: "aquaponics-system",
+    storageBucket: "aquaponics-system.appspot.com",
+    messagingSenderId: "698142730339"
+  };
+  firebase.initializeApp(config);
